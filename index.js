f336469c7d23cb561658298749436df30452dd8f
Teagan Durtschi <teagan@durtschi.com>
2023-09-10 11:46:28 -0500

    Refactoring, renaming.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// Thanks to https://www.section.io/engineering-education/session-management-in-nodejs-using-expressjs-and-express-session/
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const sessions = require("express-session");
 
 const app = express();
 const PORT = 4000;
+const THIRTY_MINUTES = 1000 * 30 * 60;
+
+// REQUIRED ENVIRONNMENT VARIABLES
 const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID;
 const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET;
 const EXPRESS_SESSIONS_SECRET = process.env.EXPRESS_SESSIONS_SECRET;
 const OIDC_REDIRECT_URL = process.env.OIDC_REDIRECT_URL;
-const THIRTY_MINUTES = 1000 * 30 * 60;
+const OIDC_ACCESS_TOKEN_URL = process.env.OIDC_ACCESS_TOKEN_URL;
+const OIDC_PROFILE_URL = process.env.OIDC_PROFILE_URL;
+const OIDC_AUTHORIZATION_URL = process.env.OIDC_AUTHORIZATION_URL;
 
-//session middleware
 app.use(
   sessions({
     secret: EXPRESS_SESSIONS_SECRET,
@@ -27,45 +30,39 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   if (req.session.profile) {
-    console.log(req.session.profile);
-    res.send(
-      `Welcome ${req.session.profile.name}, email ${req.session.profile.email}. <a href='/logout'>click to logout</a>`
-    );
-  } else res.sendFile("views/index.html", { root: __dirname });
+    res.sendFile("views/secure/index.html", { root: __dirname });
+  } else {
+    res.sendFile("views/index.html", { root: __dirname });
+  }
 });
 
-app.get("/oidc", (req, res) => {
+app.get("/login", (req, res) => {
   res.redirect(
-    `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${OIDC_CLIENT_ID}&redirect_uri=${OIDC_REDIRECT_URL}&state=foobar2&scope=openid%20profile%20email`
+    `${OIDC_AUTHORIZATION_URL}?response_type=code&client_id=${OIDC_CLIENT_ID}&redirect_uri=${OIDC_REDIRECT_URL}&state=foobar2&scope=openid%20profile%20email`
   );
 });
 
 app.get("/oidcCallback", async (req, res) => {
-  //req.query.code;
-  const authTokenResponse = await fetch(
-    "https://www.linkedin.com/oauth/v2/accessToken",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        code: req.query.code,
-        client_id: OIDC_CLIENT_ID,
-        client_secret: OIDC_CLIENT_SECRET,
-        redirect_uri: "http://localhost:4000/oidcCallback",
-      }),
-    }
-  ).then((r) => r.json());
+  const authTokenResponse = await fetch(OIDC_ACCESS_TOKEN_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code: req.query.code,
+      client_id: OIDC_CLIENT_ID,
+      client_secret: OIDC_CLIENT_SECRET,
+      redirect_uri: OIDC_REDIRECT_URL,
+    }),
+  }).then((r) => r.json());
 
   if (authTokenResponse.access_token) {
     req.session.access_token = authTokenResponse.access_token;
 
-    const profileResponse = await fetch(
-      "https://api.linkedin.com/v2/userinfo",
-      { headers: { Authorization: `Bearer ${req.session.access_token}` } }
-    ).then((r) => r.json());
+    const profileResponse = await fetch(OIDC_PROFILE_URL, {
+      headers: { Authorization: `Bearer ${req.session.access_token}` },
+    }).then((r) => r.json());
 
     req.session.profile = profileResponse;
   }
@@ -73,16 +70,11 @@ app.get("/oidcCallback", async (req, res) => {
   res.redirect("/");
 });
 
-app.get("/oidcLoggedIn", async (req, res) => {
-  if (req.session.access_token) {
-    const profileResponse = await fetch(
-      "https://api.linkedin.com/v2/userinfo",
-      { headers: { Authorization: `Bearer ${req.session.access_token}` } }
-    ).then((r) => r.json());
-
-    res.send("You're logged in! \n\n " + JSON.stringify(profileResponse));
+app.get("/profile", (req, res) => {
+  if (req.session.profile) {
+    res.send(req.session.profile);
   } else {
-    res.sendStatus(400, "Not logged in.");
+    res.sendStatus(403);
   }
 });
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Express Session Template
+
+This is a simple example which uses LinkedIn's user authentication API to grant access to a user. It uses session cookies to manage the user's state.
+
+Thanks to https://www.section.io/engineering-education/session-management-in-nodejs-using-expressjs-and-express-session/
